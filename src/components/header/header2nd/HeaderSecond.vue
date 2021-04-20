63f453b278a0ad30b70f2e71ce54b789e6987b34
nmmuchate <58500613+nmmuchate@users.noreply.github.com>
2021-04-20 07:47:44 +0200

    criação do segundo header, e adição de fontes
    Header que  não pode aparecer onde são entregadas as tasks

---
+++ src/components/header/header2nd/HeaderSecond.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="constrain">
+    <div class="row">
+      <div class="col-6 row wrap justify-start items-start">
+        <q-btn
+        to="/tasks"
+        color="orange-5"
+        icon="pending_actions"
+        flat
+        label="Task Control"
+      />
+      </div>
+      <div class="col-6 row wrap justify-end items-end">
+        <q-btn round>
+          <q-avatar size="42px">
+            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+          </q-avatar>
+        </q-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,31 +1,38 @@
 <template>
   <q-layout class="constrain" view="lHh Lpr lFf">
-    <q-header class="header">
-      <header-up/>
-      <q-separator class="constrain" inset dark/>
-      <header-md/>
-      <q-tabs
-        v-model="tab"
-        dense
-        class=" tabs constrain"
-        indicator-color="orange-7"
-        align="justify"
-        narrow-indicator
-      >
-        <q-route-tab
-          to="/tasks"
-          label="Tarefas Gerais"
-        />
-        <q-route-tab
-          to="/individualtasks"
-          label="Tarefas Individuais"
-        />
-        <q-route-tab
-          to="/perfil"
-          label="Tarefas Pessoais"
-        />
-      </q-tabs>
-    </q-header>
+    <div v-if='showHeader1'>
+      <q-header class="header">
+        <header-up/>
+        <q-separator class="constrain" inset dark/>
+        <header-md/>
+        <q-tabs
+          v-model="tab"
+          dense
+          class=" tabs constrain"
+          indicator-color="orange-7"
+          align="justify"
+          narrow-indicator
+        >
+          <q-route-tab
+            to="/tasks"
+            label="Tarefas Gerais"
+          />
+          <q-route-tab
+            to="/individualtasks"
+            label="Tarefas Individuais"
+          />
+          <q-route-tab
+            to="/personaltasks"
+            label="Tarefas Pessoais"
+          />
+        </q-tabs>
+      </q-header>
+    </div>
+    <div>
+      <q-header class="headerSCD">
+        <header-second/>
+      </q-header>
+    </div>
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -37,11 +44,22 @@
     name: 'MainLayout',
     components: {
       'header-up': () => import('../components/header/HeaderUp.vue'),
-      'header-md': () => import('../components/header/HeaderMd.vue')
+      'header-md': () => import('../components/header/HeaderMd.vue'),
+      'header-second': () => import('../components/header/header2nd/HeaderSecond.vue')
+
     },
     data() {
       return {
-        tab: 'mails'
+        tab: 'mails',
+        showHeader: true
+      }
+    },
+    computed: {
+      showHeader1(){
+        let currentPath = this.$route.fullPath
+        if (currentPath === '/tasks') return this.showHeader
+        else if (currentPath === '/individualtasks') return this.showHeader
+        else if (currentPath === '/personaltasks') return this.showHeader
       }
     }
   }
@@ -52,9 +70,15 @@
   height: 280px;
 }
 
+.headerSCD {
+  width: 1440;
+  height: 50px;
+}
 .tabs {
   margin-top: 85px;
-  position: initial;
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-style: normal;
+  font-weight: 600;
 }
 
 </style>
